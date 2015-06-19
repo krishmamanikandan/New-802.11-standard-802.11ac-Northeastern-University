daf706c4d4f6d87bb773340d56e75713602832ee
Krishma ManikandanSanthi <krishma.m.s.91@gmail.com>
2015-06-18 23:34:12 -0400

    Create rngs.h

---
+++ rngs.h
@@ -0,0 +1,17 @@
+Rngs.h
+/* -----------------------------------------------------------------------
+* Name            : rngs.h  (header file for the library file rngs.c)
+* -----------------------------------------------------------------------
+*/
+ 
+#if !defined( _RNGS_ )
+#define _RNGS_
+ 
+double Random(void);
+void   PlantSeeds(long x);
+void   GetSeed(long *x);
+void   PutSeed(long x);
+void   SelectStream(int index);
+void   TestRandom(void);
+ 
+#endif
